1d756798d821aa9b669c3038c624d3ae65b3af53
Alejandro Rivera <alejandroriveraa@proton.me>
2023-12-04 14:32:46 -0500

    Build and style brand pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,11 @@ import WhoAreWe from "./pages/who-are-we/WhoAreWe"
 import OurCoffee from "./pages/our-coffee/OurCoffee"
 import BrandTemplate from "./components/brand-template/BrandTemplate"
 import SealTemplate from "./components/seal-template/SealTemplate"
+import {useTranslation} from "react-i18next"
 
 function App() {
+  const {t} = useTranslation()
+  
   return (
     <div className="app">
       <BrowserRouter>
@@ -21,13 +24,90 @@ function App() {
               <Route path="our-coffee" element={<OurCoffee />}/>
               <Route path="seals/tueste-cubano" element={<SealTemplate />}/>
               <Route path="seals/cafe-cubano" element={<SealTemplate />}/>
-              <Route path="brands/cohiba" element={<BrandTemplate />}/>
-              <Route path="brands/montecristo" element={<BrandTemplate />}/>
-              <Route path="brands/guantanamera" element={<BrandTemplate />}/>
-              <Route path="brands/serrano" element={<BrandTemplate />}/>
-              <Route path="brands/turquino" element={<BrandTemplate />}/>
-              <Route path="brands/arriero" element={<BrandTemplate />}/>
-              <Route path="brands/prodigio" element={<BrandTemplate />}/>
+              <Route path="brands/cohiba" element={
+                <BrandTemplate 
+                backgroundColor={"--color-raisin-black"}
+                banner={"/images/banner-cohiba.jpg"}
+                logo={"/images/logo-cohiba.svg"}
+                category={t("pages.cohiba.category")}
+                video={"/videos/promo-video-compressed-whtsp.mp4"}
+                slogan={t("pages.cohiba.header")}
+                description={t("pages.cohiba.content")}
+                format={t("pages.cohiba.format")}
+                />
+              }/>
+              <Route path="brands/guantanamera" element={
+                <BrandTemplate 
+                backgroundColor={"--color-persian-plum"}
+                banner={"/images/banner-guantanamera.jpg"}
+                logo={"/images/logo-guantanamera.svg"}
+                category={t("pages.guantanamera.category")}
+                video={"/videos/promo-video-compressed-whtsp.mp4"}
+                slogan={t("pages.guantanamera.header")}
+                description={t("pages.guantanamera.content")}
+                format={t("pages.guantanamera.format")}
+                />
+              }/>
+              <Route path="brands/montecristo" element={
+                <BrandTemplate
+                backgroundColor={"--color-deep-taupe"}
+                banner={"/images/banner-montecristo.jpg"}
+                logo={"/images/logo-montecristo.svg"}
+                category={t("pages.montecristo.category")}
+                video={"/videos/promo-video-compressed-whtsp.mp4"}
+                slogan={t("pages.montecristo.header")}
+                description={t("pages.montecristo.content")}
+                format={t("pages.montecristo.format")}
+                />
+              }/>
+              <Route path="brands/serrano" element={
+                <BrandTemplate
+                backgroundColor={"--color-dark-raspberry"}
+                banner={"/images/banner-serrano.jpg"}
+                logo={"/images/logo-serrano.svg"}
+                category={t("pages.serrano.category")}
+                video={"/videos/promo-video-compressed-whtsp.mp4"}
+                slogan={t("pages.serrano.header")}
+                description={t("pages.serrano.content")}
+                format={t("pages.serrano.format")}
+                />
+              }/>
+              <Route path="brands/turquino" element={
+                <BrandTemplate
+                backgroundColor={"--color-stpatricks-blue"}
+                banner={"/images/banner-turquino.jpg"}
+                logo={"/images/logo-turquino.svg"}
+                category={t("pages.turquino.category")}
+                video={"/videos/promo-video-compressed-whtsp.mp4"}
+                slogan={t("pages.turquino.header")}
+                description={t("pages.turquino.content")}
+                format={t("pages.turquino.format")}
+                />
+              }/>
+              <Route path="brands/arriero" element={
+                <BrandTemplate
+                backgroundColor={"--color-persian-plum"}
+                banner={"/images/banner-arriero.jpg"}
+                logo={"/images/logo-arriero.svg"}
+                category={t("pages.arriero.category")}
+                video={"/videos/promo-video-compressed-whtsp.mp4"}
+                slogan={t("pages.arriero.header")}
+                description={t("pages.arriero.content")}
+                format={t("pages.arriero.format")}
+                />
+              }/>
+              <Route path="brands/prodigio" element={
+                <BrandTemplate
+                backgroundColor={"--color-old-burgundy"}
+                banner={"/images/banner-prodigio.jpg"}
+                logo={"/images/logo-prodigio.svg"}
+                category={t("pages.prodigio.category")}
+                video={"/videos/promo-video-compressed-whtsp.mp4"}
+                slogan={t("pages.prodigio.header")}
+                description={t("pages.prodigio.content")}
+                format={t("pages.prodigio.format")}
+                />
+              }/>
             </Route>
           </Route>
           <Route path="*" element={<NotFoundErrorPage/>}/>
--- src/components/brand-template/BrandTemplate.jsx
+++ src/components/brand-template/BrandTemplate.jsx
@@ -1,12 +1,47 @@
 import React from "react"
 import "./BrandTemplate.css"
 import "./BrandTemplate.responsive.css"
+import AppImage from "../app-image/AppImage"
+import AppVideo from "../app-video/AppVideo"
 
-function BrandTemplate() {
+function BrandTemplate({
+  backgroundColor,
+  banner,
+  logo,
+  category,
+  video,
+  slogan,
+  description,
+  format
+}) {
   return (
-    <>
-      {/* ... */}
-    </>
+    <div className="brand-template" style={{backgroundColor: `var(${backgroundColor})`}}>
+      <div className="brand-template__ctn ctn">
+        <div className="brand-template__ctn__banner">
+          <AppImage src={banner}/>
+        </div>
+        <div className="brand-template__ctn__logo">
+          <AppImage src={logo}/>
+        </div>
+        <div className="brand-template__ctn__category">
+          <p>{category}</p>
+        </div>
+        <div className="brand-template__ctn__video">
+          <AppVideo videoUrl={video}/>
+        </div>
+        <div className="brand-template__ctn__content">
+          <div className="brand-template__ctn__content__slogan">
+            <p>{slogan}</p>
+          </div>
+          <div className="brand-template__ctn__content__description">
+            <p>{description}</p>
+          </div>
+          <div className="brand-template__ctn__content__format">
+            <p>{format}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
